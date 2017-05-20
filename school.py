330fa487a2ac77f47f472b981a6dc4fc2d728e34
motyzk <motyzk@gmail.com>
2017-05-20 03:42:23 +0300

    some more basics

--- school.py
+++ school.py
@@ -1,6 +1,6 @@
 first_grade, second_grade, third_grade, fourth_grade, \
 first_technology_group, second_technology_group, \
-first_literature_group, second_literature_group =\
+first_literature_group, second_literature_group, new_3rd_grade_kids =\
 ({'Helen Wilson', 'Roger Miller', 'Jenifer Baldwin', 'Kim Meriwether', 'Whitney Mcelyea',
   'Roy Moore', 'David Saldivar', 'Michael Findley', 'Pamela Burris', 'Bryan Ortiz'},
 {'Cecil Massa', 'Joyce Valadez', 'Tisha Britton', 'Nancy Smith', 'Ruth Weiss', 'Carl Hale',
@@ -10,10 +10,13 @@ first_literature_group, second_literature_group =\
 {'Brittany Clement', 'Rafael Honkanen', 'Gabriel Hancock', 'Charles Stone', 'Velma Mitchell',
  'John Sales', 'Rhonda Moreno', 'Michele Trowbridge', 'Mary Warren', 'Scott Kipp'},
 {'Roger Miller', 'Jenifer Baldwin', 'Tisha Britton', 'Nancy Smith', 'Whitney Mcelyea',
- 'Ronald Tucker', 'Carl Hale', 'Michael Findley', 'Kimberly Petti', 'Pamela Burris'},
-{'Brenda Hewitt', 'Gabriel Hancock', 'Dorothy Welton', 'John Sales', 'Eunice Lawler',
- 'Dennis Galicia', 'Rhonda Moreno', 'Danny Koehler', 'Michele Trowbridge', 'Rafael Honkanen'},
+ 'Michael Findley', 'Kimberly Petti', 'Pamela Burris'},
+{'Brenda Hewitt', 'Gabriel Hancock', 'Dorothy Welton', 'John Sales', 'Eunice Lawler', 'Scott Kipp',
+ 'Dennis Galicia', 'Rhonda Moreno', 'Danny Koehler', 'Michele Trowbridge', 'Rafael Honkanen', 'Mary Warren'},
 {'Helen Wilson', 'Cecil Massa', 'Joyce Valadez', 'Kim Meriwether', 'Roy Moore', 'Ruth Weiss',
  'David Saldivar', 'Robin Gutkowski', 'Rafael Dinger', 'Bryan Ortiz'},
 {'Larry Coffin', 'Brittany Clement', 'Douglas Pardo', 'Charles Stone', 'Velma Mitchell',
- 'Tammy Vilven', 'Charlie Berger', 'Mary Warren', 'Brett Mclean', 'Scott Kipp'})
+ 'Tammy Vilven', 'Charlie Berger', 'Mary Warren', 'Brett Mclean', 'Scott Kipp'},
+{('Kobi Cohen', frozenset(['literature', 'technology'])),
+ ('Israel Israeli', frozenset(['sports', 'technology'])),
+ ('Levi Levi', frozenset(['literature', 'technology']))})
--- school_functions.py
+++ school_functions.py
@@ -1,7 +1,9 @@
 import school
 
 
-# this function should return a list with the names of the pupils in the given grade, ordered by alphabetical order
+# this function should return a list with the names of the pupils in the given grade,
+# ordered by alphabetical order.
+# input - a set including the names of the pupils in the grade
 def grade_in_alphabetical_order(grade):
     # ----- ENTER SOME CODE HERE --------
     return sorted(grade)
@@ -17,8 +19,9 @@ assert grade_in_alphabetical_order(school.fourth_grade) == \
 
 
 # This year, pupils who are either in the fourth grade or study in one of the two literature classes,
-# will go to the literature trip. return a list list of the pupils who are going to the trip,
+# will go to the literature trip. return a list of the pupils who are going to the trip,
 # ordered by alphabetical order
+# input - pupils sets.
 def going_to_the_trip(fourth_grade, first_literature_group, second_literature_group):
     # ----- ENTER SOME CODE HERE --------
     return sorted(fourth_grade | first_literature_group | second_literature_group)
@@ -32,3 +35,88 @@ assert going_to_the_trip(school.fourth_grade, school.first_literature_group, sch
         'Michele Trowbridge', 'Rafael Dinger', 'Rafael Honkanen', 'Rhonda Moreno', 'Robin Gutkowski',
         'Roy Moore', 'Ruth Weiss', 'Scott Kipp', 'Tammy Vilven', 'Velma Mitchell']
 
+
+# An external teacher is coming to lecture in our school, to pupils who study in
+# both a technology group and a literature group.
+# As 1st & 2nd grade pupils can go to none, one, or both of the first literature
+# group and the first technology group,
+# and 3rd & 4th grade pupils can go to none, one, or both of the second literature
+# group and the second technology group,
+# you are going to write a function that returns a list with the names of the pupils
+# who take both literature and technology classes.
+# input - sets, each one corresponding to its name.
+def take_both_extra_classes(first_literature_group, second_literature_group,
+                            first_technology_group, second_technology_group):
+    # ----- ENTER SOME CODE HERE --------
+    return sorted((first_technology_group & first_literature_group) |
+                  (second_technology_group & second_literature_group))
+    # -----------------------------------
+
+
+assert take_both_extra_classes(school.first_literature_group,
+                               school.second_literature_group,
+                               school.first_technology_group,
+                               school.second_technology_group) == ['Mary Warren', 'Scott Kipp']
+
+
+# The principle will lecture to pupils who take no extra classes (literature, technology)
+# about the importance of expanding your horizons.
+# write a function that returns a sorted list of the names of the pupils who take no extra classes.
+# # input - sets, each one corresponding to its name.
+def take_no_extra_classes(first_grade, second_grade, third_grade, fourth_grade,
+                          first_technology_group, second_technology_group,
+                          first_literature_group, second_literature_group):
+    # ----- ENTER SOME CODE HERE --------
+    return sorted(((first_grade | second_grade) - (first_technology_group | first_literature_group)) |
+                  ((third_grade | fourth_grade) - (second_technology_group | second_literature_group)))
+    # -----------------------------------
+
+
+assert take_no_extra_classes(school.first_grade, school.second_grade, school.third_grade, school.fourth_grade,
+            school.first_technology_group, school.second_technology_group,
+            school.first_literature_group, school.second_literature_group) == ['Carl Hale', 'Ronald Tucker']
+
+
+# return a sorted list of the pupils who take axactly one extra class.
+# input - sets, each one corresponding to its name.
+def take_one_extra_class(first_literature_group, second_literature_group,
+                         first_technology_group, second_technology_group):
+    # ----- ENTER SOME CODE HERE --------
+    return sorted((first_literature_group ^ first_technology_group) |
+                  (second_literature_group ^ second_technology_group))
+    # -----------------------------------
+
+
+assert take_one_extra_class(school.first_literature_group, school.second_literature_group,
+                            school.first_technology_group, school.second_technology_group) == \
+       ['Brenda Hewitt', 'Brett Mclean', 'Brittany Clement', 'Bryan Ortiz', 'Cecil Massa', 'Charles Stone',
+        'Charlie Berger', 'Danny Koehler', 'David Saldivar', 'Dennis Galicia', 'Dorothy Welton', 'Douglas Pardo',
+        'Eunice Lawler', 'Gabriel Hancock', 'Helen Wilson', 'Jenifer Baldwin', 'John Sales', 'Joyce Valadez',
+        'Kim Meriwether', 'Kimberly Petti', 'Larry Coffin', 'Michael Findley', 'Michele Trowbridge', 'Nancy Smith',
+        'Pamela Burris', 'Rafael Dinger', 'Rafael Honkanen', 'Rhonda Moreno', 'Robin Gutkowski', 'Roger Miller',
+        'Roy Moore', 'Ruth Weiss', 'Tammy Vilven', 'Tisha Britton', 'Velma Mitchell', 'Whitney Mcelyea']
+
+
+# some new kids got to town and want to go to our school's 3rd grade,
+# but we can only accept them if we teach the extra classes they used
+# to take back in their previous schools.
+# return a set of the pupils in 3rd grade, after we accept the pupils we can.
+# input -
+# third_grade - a set, including the names of all the pupils in the 3rd grade
+# new_kids - a set of tuples, each represents a kid and consists of 2 entries: the first
+# one is the name of the kid(string) and the second one is a set of
+# the extra classes he takes.
+# input - sets, each one corresponding to its name.
+def add_new_pupils(third_grade, new_kids):
+    # ----- ENTER SOME CODE HERE --------
+    for name, extra_lessons in new_kids:
+        if extra_lessons <= {'literature', 'technology'}:
+            third_grade.add(name)
+    return third_grade
+    # -----------------------------------
+
+
+assert add_new_pupils(school.third_grade, school.new_3rd_grade_kids) == \
+       {'Kobi Cohen', 'Charlie Berger', 'Douglas Pardo', 'Brett Mclean', 'Levi Levi',
+        'Danny Koehler', 'Dorothy Welton', 'Eunice Lawler', 'Dennis Galicia', 'Brenda Hewitt',
+        'Larry Coffin', 'Tammy Vilven'}
